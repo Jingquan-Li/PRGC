bfacc0780a346a28bd45080ca38f25c1cd0c41fb
Jingquan-Li <56573623+Jingquan-Li@users.noreply.github.com>
2022-10-12 21:39:05 +0800

    Update roary.sh

--- Scripts/roary.sh
+++ Scripts/roary.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+####roary install
+#install mamba 
+conda install -c conda-forge mamba
 
+conda config --add channels r
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+mamba install roary
